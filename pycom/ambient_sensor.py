b13c1f1866214a9e4003dfa928ddc5f86d469605
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2020-05-11 11:37:27 +0200

    threshold removed from sensor

--- pycom/ambient_sensor.py
+++ pycom/ambient_sensor.py
@@ -8,12 +8,12 @@ class AmbientSensor:
         # Initialize the sensor
         integration_time = LTR329ALS01.ALS_INT_100
         measurement_rate = LTR329ALS01.ALS_RATE_500 
-        gain = LTR329ALS01.ALS_GAIN_8X
+        gain = LTR329ALS01.ALS_GAIN_1X
         self.__lightsensor = LTR329ALS01(integration=integration_time, rate=measurement_rate, gain=gain)
         self.__is_sampling = False
 
         # Set default sample rate
-        self.__sample_rate = 0.2
+        self.__sample_rate = 1
 
         # Threshold for when a sample should be emitted
         self.__threshold = threshold
@@ -35,9 +35,9 @@ class AmbientSensor:
                 currentSample = self.__lightsensor.light()
 
                 # Should we emit?
-                if (abs(lastSample[0] - currentSample[0]) > self.__threshold or abs(lastSample[1] - currentSample[1]) > self.__threshold):
-                    lastSample = currentSample
-                    self.__emit(lastSample)
+                #if (abs(lastSample[0] - currentSample[0]) > self.__threshold or abs(lastSample[1] - currentSample[1]) > self.__threshold):
+                lastSample = currentSample
+                self.__emit(lastSample)
                 
                 time.sleep(self.__sample_rate)
 
