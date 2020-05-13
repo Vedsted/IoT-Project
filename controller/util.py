8df43282f6ee70202d4712d291cbf41876225250
Christian Clausen <csbc92@hotmail.com>
2020-05-13 14:25:42 +0200

    Added message count to controller

--- controller/util.py
+++ controller/util.py
@@ -19,3 +19,14 @@ def map_intensity_to_rgb(state):
     b = math.floor((state['ref_blue']/intensity_max)*intensity)
     return (r,g,b)
 
+def increment_mqtt_message_send_count():
+    # Read the current content of the file (if it exists)
+    filename = "mqtt_messages_sent_counter.state"
+    myfile = open(filename, 'r')
+    current_counter = int(myfile.readline())
+    myfile.close()
+    new_counter = current_counter + 1
+    myfile = open(filename, 'w')
+    myfile.write(str(new_counter))
+    myfile.close()
+    return new_counter
